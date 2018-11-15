7ae8d088842237b57c11a041f1fea8c2910e7849
seongwonhan88 <seongwonhan88@gmail.com>
2018-11-15 21:07:55 +0900

    소셜 로그인 연결(페이스북, 구글)완료, 로그아웃 뷰 생성

--- app/members/views.py
+++ app/members/views.py
@@ -1,8 +1,17 @@
-from django.shortcuts import render
+from django.contrib.auth import logout
+from django.shortcuts import render, redirect
 
 
 def login_view(request):
     return render(request, 'members/login.html')
+
+
+def logout_view(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('shows:show-list')
+    else:
+        pass
 
 
 # 사용자가 찜한 목록 보
