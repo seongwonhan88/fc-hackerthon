bf27f290287a134624765ec4a9f9c03e9e788598
seongwonhan88 <seongwonhan88@gmail.com>
2018-11-16 11:50:24 +0900

    user_pick.html로 form 전송 완료

--- app/members/urls.py
+++ app/members/urls.py
@@ -7,6 +7,5 @@ app_name = 'members'
 urlpatterns = [
     path('login/', views.login_view, name='login-view'),
     path('logout/', views.logout_view, name='logout-view'),
-    # pk = user pk
     path('user_pick/', views.user_pick, name='user-pick'),
 ]
